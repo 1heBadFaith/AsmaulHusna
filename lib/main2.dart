f34d5b8989d58496d7f571603068d73d42eb91e7
1heBadFaith <azhardevelop@gmail.com>
2019-10-21 14:06:35 +0700

    Initial commit

---
+++ lib/main2.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(MyAsmaulHusna());
+
+
+
+class MyAsmaulHusna extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TampilanUtama(),
+    );
+  }
+}
+
+class TampilanUtama  extends StatefulWidget {
+  @override
+  _TampilanUtamaState createState() => _TampilanUtamaState();
+}
+
+class _TampilanUtamaState extends State<TampilanUtama > {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
